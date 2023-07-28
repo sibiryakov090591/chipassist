ebd7728fa7e940a7066295684325d7908a96465c
andrey <andrey.sibiriakov@chipassist.com>
2023-07-28 20:25:56 +0300

    hid tour for prod

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import constants from "@src/constants/constants";
-import { ID_ICSEARCH, ID_MASTER, ID_SUPPLIER_RESPONSE } from "@src/constants/server_constants";
+import { ID_ICSEARCH, ID_SUPPLIER_RESPONSE } from "@src/constants/server_constants";
 import { Box, Container, Grid, Hidden } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import InstagramIcon from "@material-ui/icons/Instagram";
@@ -133,9 +133,9 @@ const Footer = () => {
                     <NavLink className={classes.navLink} to={"/privacy_policy"}>
                       Privacy Policy
                     </NavLink>
-                      <NavLink className={classes.navLink} to={"/rfq-list-quotes"}>
-                        RFQ List
-                      </NavLink>
+                    <NavLink className={classes.navLink} to={"/rfq-list-quotes"}>
+                      RFQ List
+                    </NavLink>
                     {!isSupplierResponse && (
                       <NavLink className={classes.navLink} to={"/FAQ"}>
                         FAQ
