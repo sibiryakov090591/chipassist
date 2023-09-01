75986926db2cd95b1bbd87f5ad8e4752609d513b
NikitaEfimov0 <nikitaef2002@gmail.com>
2023-09-01 11:36:57 +0300

    MPN-field-fix

--- src/views/chipassist/RfqList/components/Form/RFQListForm.tsx
+++ src/views/chipassist/RfqList/components/Form/RFQListForm.tsx
@@ -470,7 +470,7 @@ export const RFQListForm = () => {
     return rfqListState.values
       .filter((elem) => elem.MPN !== "" && elem.quantity !== "")
       .map((rfq) => ({
-        part_number: rfq.MPN,
+        part_number: rfq.MPN.replace(/\s/g, ""),
         manufacturer: rfq.manufacturer,
         quantity: rfq.quantity,
         price: rfq.price !== "" ? rfq.price : 0,
--- src/views/chipassist/RfqList/components/PartNumberInput/PartNumberInput.tsx
+++ src/views/chipassist/RfqList/components/PartNumberInput/PartNumberInput.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Autosuggest from "react-autosuggest";
-import { TextField } from "@material-ui/core";
+import { TextField, Box } from "@material-ui/core";
 import useAppDispatch from "@src/hooks/useAppDispatch";
 import { onSuggestionsClearRequested, onSuggestionsFetchRequested } from "@src/store/suggestions/suggestionsActions";
 import useAppSelector from "@src/hooks/useAppSelector";
+import Popper from "@material-ui/core/Popper";
 import { useStyles } from "./styles";
 
 interface Props {
@@ -33,12 +34,15 @@ const PartNumberInput: React.FC<Props> = ({
   const suggestions = useAppSelector((state) => state.suggestions.suggestions);
   const [hasFocus, setHasFocus] = useState(false);
   const dispatch = useAppDispatch();
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const onSuggestionSelected = (e: any, { suggestionValue }: any) => {
     onChange({ ...e, target: { ...e.target, value: suggestionValue, name: "MPN" } });
   };
 
   const onSuggestionsFetchRequestedHandler = (e: any) => {
+    e.value = e.value.replace(/[^a-zA-Z0-9!@#$%^&*)(-_=+.,?№;:/]/g, "");
     dispatch(onSuggestionsFetchRequested(e.value));
   };
 
@@ -50,16 +54,37 @@ const PartNumberInput: React.FC<Props> = ({
 
   const getSuggestionValue = (suggestion: any) => suggestion.name;
 
-  const onFieldFocus = () => {
+  const onFieldFocus = (e: any) => {
+    setAnchorEl(e.target);
+    setShowTooltip(false);
     setHasFocus(true);
   };
 
   const onFieldBlur = () => {
     blurHandler();
+    // if (e.target.value.indexOf(" ") !== -1) {
+    //   e.target.value = e.target.value.replace(/\s/g, "");
+    //   onChange(e);
+    //   if (timer.current) {
+    //     clearTimeout(timer.current);
+    //   }
+    //   timer.current = setTimeout(() => {
+    //     setShowTooltip(true);
+    //     if (closeTimer.current) {
+    //       clearTimeout(closeTimer.current);
+    //     }
+    //     closeTimer.current = setTimeout(() => {
+    //       setShowTooltip(false);
+    //     }, 2000);
+    //   }, 600);
+    // }
     setHasFocus(false);
   };
 
   const onFieldChange = (e: any) => {
+    const prevValue = e.target.value;
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9# /-]/g, "");
+    setShowTooltip(prevValue !== e.target.value);
     onChange(e);
   };
 
@@ -74,7 +99,7 @@ const PartNumberInput: React.FC<Props> = ({
         getSuggestionValue={getSuggestionValue}
         focusInputOnSuggestionClick={false}
         renderSuggestion={renderSuggestion}
-        shouldRenderSuggestions={() => hasFocus}
+        shouldRenderSuggestions={() => hasFocus && !showTooltip}
         inputProps={{
           value: value || "",
           onChange: onFieldChange,
@@ -82,22 +107,31 @@ const PartNumberInput: React.FC<Props> = ({
           onBlur: onFieldBlur,
         }}
         renderInputComponent={(inputProps: any) => (
-          <TextField
-            title={partnumberRef && partnumberRef.length > 25 ? partnumberRef : ""}
-            disabled={disabled}
-            variant={"outlined"}
-            name={"MPN"}
-            label={"Part number *"}
-            placeholder={"ex. KNP100"}
-            size="small"
-            fullWidth
-            InputLabelProps={{
-              shrink: true,
-            }}
-            className={classes.rfqInput}
-            {...inputProps}
-            {...errorHandler}
-          />
+          <>
+            <TextField
+              title={partnumberRef && partnumberRef.length > 25 ? partnumberRef : ""}
+              aria-describedby={"mpn_field"}
+              disabled={disabled}
+              variant={"outlined"}
+              name={"MPN"}
+              label={"Part number *"}
+              placeholder={"ex. KNP100"}
+              size="small"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              className={classes.rfqInput}
+              {...inputProps}
+              {...errorHandler}
+            />
+            <Popper id={"mpn_field"} open={showTooltip} anchorEl={anchorEl}>
+              {/* Spaces were successfully deleted */}
+              <Box display="flex" alignItems={"center"} justifyContent={"space-between"}>
+                <span className={classes.error}>{'Only "a-z, A-Z, 0-9, /, #, -, ." are allowed in MPN'}</span>
+              </Box>
+            </Popper>
+          </>
         )}
       />
     </div>
