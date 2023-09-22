f7b09d4710a5fbe916880e5d1b749ea915ef87d0
andrey <andrey.sibiriakov@chipassist.com>
2023-09-22 12:59:39 +0300

    fixed showing RFQList modal

--- src/views/chipassist/Rfq/components/RFQForm/RFQForm.tsx
+++ src/views/chipassist/Rfq/components/RFQForm/RFQForm.tsx
@@ -528,6 +528,7 @@ const RFQForm: React.FC<Props> = ({ onCloseModalHandler }) => {
     dispatch(changeMisc("rfq", formState.values, formState.values.email));
 
     localStorage.setItem("before_unload_alert_disabled", "true");
+    localStorage.setItem("product_request_hint_disabled", "true");
     if (isAuthenticated) {
       dispatch(saveRfqItem(data)).then(() => {
         batch(() => {
