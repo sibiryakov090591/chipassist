6b70f7095039a5b0686a697ddf61777b69413f30
andrey <andrey.sibiriakov@chipassist.com>
2023-10-03 20:20:52 +0300

    fixed errors messages

--- src/views/chipassist/Chat/components/ChatWindow/components/SendOrderModal/SendOrderModal.tsx
+++ src/views/chipassist/Chat/components/ChatWindow/components/SendOrderModal/SendOrderModal.tsx
@@ -332,7 +332,7 @@ const SendOrderModal: React.FC<Props> = ({ open, stock, onCloseModal, setIsSendi
                 rules={{
                   required: {
                     value: true,
-                    message: "Required",
+                    message: "Address is required",
                   },
                 }}
                 render={({ field }) => (
@@ -382,7 +382,7 @@ const SendOrderModal: React.FC<Props> = ({ open, stock, onCloseModal, setIsSendi
                   rules={{
                     required: {
                       value: true,
-                      message: "Required",
+                      message: "Qty is required",
                     },
                     min: {
                       value: 1,
