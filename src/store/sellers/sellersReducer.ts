9009e072480c6e4767ad52b1ae255dd8c1621f00
NikitaEfimov0 <nikitaef2002@gmail.com>
2023-10-17 18:36:12 +0300

    fixed bug with button

--- src/store/sellers/sellersReducer.ts
+++ src/store/sellers/sellersReducer.ts
@@ -2,6 +2,7 @@ import * as actionTypes from "./sellersTypes";
 
 const initialState: actionTypes.SellersState = {
   items: [],
+  isLoading: true,
 };
 
 export default (state = initialState, { type, payload }: any) => {
@@ -10,6 +11,12 @@ export default (state = initialState, { type, payload }: any) => {
       return { ...state, items: payload.results };
     case actionTypes.JOIN_ITEMS:
       return { ...state, items: [...state.items, ...payload.results] };
+    case actionTypes.ITEMS_FETCHING_R:
+      return { ...state, isLoading: true };
+    case actionTypes.ITEMS_FETCHING_S:
+      return { ...state, isLoading: false };
+    case actionTypes.ITEMS_FETCHING_F:
+      return { ...state, isLoading: false };
     default:
       return state;
   }
