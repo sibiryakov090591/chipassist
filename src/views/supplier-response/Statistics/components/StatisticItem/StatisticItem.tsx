e1cf865348b703261d174a5706ffcca3ac2db20b
andrey <andrey.sibiriakov@chipassist.com>
2023-12-13 09:50:43 -0600

    fixed default currency

--- src/views/supplier-response/Statistics/components/StatisticItem/StatisticItem.tsx
+++ src/views/supplier-response/Statistics/components/StatisticItem/StatisticItem.tsx
@@ -75,7 +75,7 @@ const StatisticItem: React.FC<Props> = ({ items, index }) => {
               [classes.biggerPrice]: !!item.price && !!item.position?.includes("+"),
             })}
           >
-            {item.price ? formatMoney(currencyPrice(item.price, "EUR")) : "-"}
+            {item.price ? formatMoney(currencyPrice(item.price, "USD")) : "-"}
           </div>
         </TableCell>
         <TableCell>
@@ -86,7 +86,7 @@ const StatisticItem: React.FC<Props> = ({ items, index }) => {
               [classes.biggerPrice]: !!item.competitive_price && !item.position?.includes("+"),
             })}
           >
-            {item.competitive_price ? formatMoney(currencyPrice(item.competitive_price, "EUR")) : "-"}
+            {item.competitive_price ? formatMoney(currencyPrice(item.competitive_price, "USD")) : "-"}
           </div>
         </TableCell>
         <TableCell>
