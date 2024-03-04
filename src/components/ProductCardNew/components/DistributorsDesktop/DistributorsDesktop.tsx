08f9e5b9b2d02f5aba34a17cacecac26bc39e83c
NikitaEfimov0 <nikitaef2002@gmail.com>
2024-03-04 16:08:55 +0300

    fixed logic of getting mpq

--- src/components/ProductCardNew/components/DistributorsDesktop/DistributorsDesktop.tsx
+++ src/components/ProductCardNew/components/DistributorsDesktop/DistributorsDesktop.tsx
@@ -115,15 +115,18 @@ const DistributorsDesktop: React.FC<Props> = ({
            *  Create combinedDataItem for show in collapsed group condition
            *  Contains the best values
            */
+          let minSr: SortedStockrecord = null;
           const combinedDataItem = group.reduce(
             (acc, sr) => {
               const accDate = new Date(acc.date_updated.replace(/ /g, "T"));
               const srDate = new Date(sr.date_updated.replace(/ /g, "T"));
+              if (!minSr) minSr = sr;
+              minSr = sr.moq < acc.moq ? sr : minSr;
+
               return {
                 ...acc,
                 num_in_stock: Math.max(acc.num_in_stock, sr.num_in_stock),
-                moq: sr.num_in_stock ? Math.min(acc.moq, sr.moq) : acc.moq,
-                mpq: sr.num_in_stock ? (Math.min(acc.moq, sr.moq) ? sr.mpq : acc.mpq) : acc.mpq,
+                moq: Math.min(acc.moq, sr.moq),
                 date_updated: accDate.getTime() < srDate.getTime() ? acc.date_updated : sr.date_updated,
                 prices: acc.prices.length ? acc.prices : sr.prices,
                 price_1: acc.price_1 ? (sr.price_1 ? Math.min(acc.price_1, sr.price_1) : acc.price_1) : sr.price_1,
@@ -151,7 +154,7 @@ const DistributorsDesktop: React.FC<Props> = ({
             },
             { ...group[0] },
           );
-          return [combinedDataItem, ...group];
+          return [{ ...combinedDataItem, mpq: minSr.mpq }, ...group];
         });
       setStockrecords(sortFn(res, sortBy.name, sortBy.direction));
     }
